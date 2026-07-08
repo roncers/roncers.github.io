cc0cf4d1b5e11dc9d126286eb72a9feca9a11ed7
Martín <martin.roncero.l@gmail.com>
2026-07-08 20:37:48 +0200

    feat: video renderer adition

--- src/components/tabs/misc/TearsInTheRain.tsx
+++ src/components/tabs/misc/TearsInTheRain.tsx
@@ -1,17 +1,11 @@
 import type { TabComponentProps } from "@/types/tab.types"
 import PoppingWindow from "@/components/commons/poping-window/PoppingWindow"
-// TODO: create a video embedder for reutilization purposes.
+import VideoRenderer from "@/components/commons/VideoRenderer"
+// TODO: Pause the video immediately when closing the tab.
 export default function TearsInTheRain(props: TabComponentProps) {
     return (
         <PoppingWindow {...props}>
-            <iframe
-                style={{ display: "block", border: "none", width: "100%", height: "100%" }}
-                width="100%"
-                height="100%"
-                src="https://www.youtube.com/embed/HU7Ga7qTLDU?autoplay=1&controls=0"
-                title="Tears in the Rain"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            />
+            <VideoRenderer embedUrl="HU7Ga7qTLDU" title="Tears in the Rain" />
         </PoppingWindow>
     )
 }
