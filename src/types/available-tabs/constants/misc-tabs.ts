ad75b09161ad9779cf773372ba942f4f791b1ae3
Martín <martin.roncero.l@gmail.com>
2026-07-08 20:34:09 +0200

    fix: real release date of blade runner

--- src/types/available-tabs/constants/misc-tabs.ts
+++ src/types/available-tabs/constants/misc-tabs.ts
@@ -6,7 +6,7 @@ export const MISC_TABS = {
     loader: () => import("@/components/tabs/misc/TearsInTheRain").then((m) => m.default),
     i18key: "info.miscellaneous.tearsIntheRain",
     type: TAB_TYPES.VIDEO,
-    date: new Date("2026-07-07 18:46"),
+    date: new Date("1982-06-25 16:07"),
     size: 17,
   },
 } as const satisfies Record<string, TabDefinition>
