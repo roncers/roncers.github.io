9e23a6e3025732c8e986a0c9fe19230570be1c1b
Martín <martin.roncero.l@gmail.com>
2026-07-10 18:18:05 +0200

    fix: error in terminal with icon

--- src/components/icons/HideIcon.tsx
+++ src/components/icons/HideIcon.tsx
@@ -1,5 +1,6 @@
 import type { SVGProps } from 'react';
 
+// million-ignore
 export function Subtract(props: SVGProps<SVGSVGElement>) {
   return (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="currentColor" d="M8 15h16v2H8z" /></svg>
--- src/components/icons/MinimizeIcon.tsx
+++ src/components/icons/MinimizeIcon.tsx
@@ -1,5 +1,6 @@
 import type { SVGProps } from 'react';
 
+// million-ignore
 export function Minimize(props: SVGProps<SVGSVGElement>) {
   return (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="currentColor" d="M4 18v2h6.586L2 28.582L3.414 30L12 21.414V28h2V18zM30 3.416L28.592 2L20 10.586V4h-2v10h10v-2h-6.586z" /></svg>
