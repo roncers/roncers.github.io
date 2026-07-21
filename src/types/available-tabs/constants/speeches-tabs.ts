7a4c4ba5afc74e41cc876287b3e7f59e51b05970
Martin Roncero <mroncero@indra.es>
2026-07-21 16:43:37 +0200

    feat: video generic renderer

--- src/types/available-tabs/constants/speeches-tabs.ts
+++ src/types/available-tabs/constants/speeches-tabs.ts
@@ -2,21 +2,24 @@ import { TAB_TYPES, type TabDefinition } from "../commons.types"
 
 export const SPEECHES_TABS = {
   TEARS_IN_THE_RAIN: {
-    loader: () => import("@/components/tabs/misc/speeches/TearsInTheRain").then((m) => m.default),
+    loader: () => import("@/components/tabs/generic/Video").then((m) => m.default),
+    args: [{ es: "qKpMFMiRkBI", en: "HU7Ga7qTLDU" }],
     i18key: "info.miscellaneous.speeches.tearsIntheRain",
     type: TAB_TYPES.VIDEO,
     date: new Date("1982-06-25 16:07"),
     size: 2760,
   },
   OUR_TIME_HAS_PASSED: {
-    loader: () => import("@/components/tabs/misc/speeches/OurTimeHasPassed").then((m) => m.default),
+    loader: () => import("@/components/tabs/generic/Video").then((m) => m.default),
+    args: [{ es: "4IWN-6zBO2Q", en: "4IWN-6zBO2Q" }],
     i18key: "info.miscellaneous.speeches.ourTimeHasPassed",
     type: TAB_TYPES.VIDEO,
     date: new Date("2010-05-18 16:09"),
     size: 2280,
   },
   SWEAT_OF_HIS_BROW: {
-    loader: () => import("@/components/tabs/misc/speeches/SweatOfHisBrow").then((m) => m.default),
+    loader: () => import("@/components/tabs/generic/Video").then((m) => m.default),
+    args: [{ es: "9_KMoUkX_os", en: "hyur9r0ekZY" }],
     i18key: "info.miscellaneous.speeches.sweatOfHisBrow",
     type: TAB_TYPES.VIDEO,
     date: new Date("2007-08-21 23:23"),
