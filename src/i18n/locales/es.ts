c4f84a28ab58b2be4aee500fc644a008e037190d
Martín <martin.roncero.l@gmail.com>
2026-07-21 23:18:13 +0200

    feat: glsl spinner

--- src/i18n/locales/es.ts
+++ src/i18n/locales/es.ts
@@ -13,6 +13,7 @@ const es: DeepString<typeof en> = {
     ringsZooming: "Anillos",
     movingFloor: "Suelo movil",
     romboidRainbow: "Romboide arcoíris",
+    spinner: "Spinner",
   },
   info: {
     title: "Mi info",
@@ -23,7 +24,7 @@ const es: DeepString<typeof en> = {
       contactMe: "Contacta conmigo",
       title: "Mi info",
       header: "Soy Martín", 
-      description: "Me gusta crear.",
+      description: "Me gusta, crear.",
       linksDescription: "Aquí tienes algunos de mis proyectos:",
     },
     projects: {
---
+++ src/p5/glsl/spinner.glsl.ts
@@ -0,0 +1,27 @@
+import type p5 from "p5";
+import vertexShader from "@/p5/glsl/raw-code/spinner/spinner.vert?raw"
+import fragmentShader from "@/p5/glsl/raw-code/spinner/spinner.frag?raw"
+
+export default function sketch(p: p5, parent: HTMLElement): void {
+  let shader: p5.Shader;
+
+  p.setup = () => {
+    p.createCanvas(parent.clientWidth, parent.clientHeight, p.WEBGL);
+    p.noStroke();
+
+    shader = p.createShader(vertexShader, fragmentShader);
+  };
+
+  p.draw = () => {
+    p.shader(shader);
+
+    shader.setUniform("u_resolution", [p.width, p.height]);
+    shader.setUniform("u_time", p.millis() / 1000.0);
+
+    p.rect(-p.width / 2, -p.height / 2, p.width, p.height);
+  };
+
+  p.windowResized = () => {
+    p.resizeCanvas(parent.clientWidth, parent.clientHeight);
+  };
+}
