8921a4675fce6ced66e37a4c6c06dc6a005289ba
Martin Roncero <mroncero@indra.es>
2026-07-23 14:38:54 +0200

    fix: deleting duplicated import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { ReactElement } from "react"
-import "./assets/css/index.css"
 import P5Canvas from "./components/commons/P5Canvas"
 import nameSketch from "./p5/base/name.p5"
 import TabRenderer from "@/components/TabRenderer"
 import TaskManagerDock from "@/components/task-manager-dock/TaskManagerDock"
+import "./App.css"
 
 function App(): ReactElement {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/css/index.css'
-import "./App.css"
 import App from './App'
 import StoreProvider from './components/stores/StoreProvider'
 
